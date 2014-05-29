03ba5acf389b3da6f733fe49d67ce95c886614d6
Aaron Olkin <aaronolkin@gmail.com>
2014-05-28 21:38:12 -0400

    Added favicon and url pattern

--- mercury/urls.py
+++ mercury/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
 
+from django.conf import settings
+
 from config import config
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url=config.get("main_url","/admin/config/config/add/?key=main_url"))),
+    url(r'^$', RedirectView.as_view(url=config.get("main_url","/admin/config/config/main_url/"))),
+
+    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL+"favicon.ico")),
 
     url(r'^voting/', include('voting.urls')),
 
