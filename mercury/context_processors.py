2f053012004bb85553e869d5543d40ad21580b23
Aaron Olkin <aaronolkin@gmail.com>
2014-09-28 17:51:52 -0400

    Fixed bug with 404 errors in context processor...hopefully...

--- mercury/context_processors.py
+++ mercury/context_processors.py
@@ -4,13 +4,16 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 
 def base(request):
-    return { 
-        "jsfilename": request.resolver_match.url_name.replace(".views.","/"),
-        "viewname": request.resolver_match.url_name.rpartition(".")[2],
-        "wsaddress": mark_safe(('"ws://"+location.hostname+":5001/{}/"' if settings.DEBUG else
-                                '"ws://"+location.host+"/ws/{}/"').format(
-                                    request.resolver_match.url_name.partition(".")[0])),
-    }
+    if request.resolver_match:
+        return { 
+            "jsfilename": request.resolver_match.url_name.replace(".views.","/"),
+            "viewname": request.resolver_match.url_name.rpartition(".")[2],
+            "wsaddress": mark_safe(('"ws://"+location.hostname+":5001/{}/"' if settings.DEBUG else
+                                    '"ws://"+location.host+"/ws/{}/"').format(
+                                        request.resolver_match.url_name.partition(".")[0])),
+        }
+    else:
+        return {}
 
 def sidebar(request):
     return {}
