70b6f8dfc8c37746031e49652301dccea9f8bcd8
Aaron Olkin <aaronolkin@gmail.com>
2015-05-22 15:50:58 -0400

    Hopefully a bugfix in context_processor

--- mercury/context_processors.py
+++ mercury/context_processors.py
@@ -5,6 +5,9 @@ from django.utils.safestring import mark_safe
 
 def base(request):
     if request.resolver_match:
+        if (not request.resolver_match.url_name) and (request.resolver_match.func):
+            request.resolver_match.url_name = "{}.{}".format(request.resolver_match.func.__module__,
+                                                             request.resolver_match.func.__name__)
         return { 
             "jsfilename": request.resolver_match.url_name.replace(".views.","/"),
             "viewname": request.resolver_match.url_name.rpartition(".")[2],
